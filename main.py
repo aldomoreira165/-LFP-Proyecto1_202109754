3f9b127a8dec016ecf4f5c13bb8725c25b4a5d98
aldomoreira165 <aldovasquez2014@gmail.com>
2022-09-07 00:20:04 -0600

    manejo de archivos e interfaz terminada

---
+++ main.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+from tkinter import scrolledtext as st
+import sys
+from tkinter.font import BOLD
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb 
+
+class VentanaPrincipal:
+    
+    def __init__(self):
+        self.ventana = tk.Tk()
+        self.agregar_menu()
+        self.scrolledtext1 = st.ScrolledText(self.ventana, width=50, height=20)
+        self.scrolledtext1.grid(column=0, row=0, padx=10, pady=10)
+        self.ventana.mainloop()
+        self.nombre_archivo = None
+        
+    def agregar_menu(self):
+        barra_menus = tk.Menu(self.ventana)
+        
+        #cascada de menu archivo
+        menu_archivo = tk.Menu(barra_menus, tearoff=False)
+        menu_archivo.add_command(label="Abrir", command=self.abrir)
+        menu_archivo.add_command(label="Guardar", command=self.guardar)
+        menu_archivo.add_command(label="Guardar Como", command=self.guardar_como)
+        menu_archivo.add_command(label="Analizar")
+        menu_archivo.add_command(label="Errores")
+        menu_archivo.add_command(label="Salir", command=self.salir)
+        barra_menus.add_cascade(menu=menu_archivo, label="Archivo")
+        
+        #cascada de menu ayuda
+        menu_ayuda = tk.Menu(barra_menus, tearoff=False)
+        menu_ayuda.add_command(label="Manual de Usuario")
+        menu_ayuda.add_command(label="Manual Técnico")
+        menu_ayuda.add_command(label="Temas de Ayuda")
+        barra_menus.add_cascade(menu=menu_ayuda, label="Ayuda")
+        
+        self.ventana.config(menu=barra_menus)
+
+    def salir(self):
+        sys.exit(0)
+        
+    def guardar_como(self):
+        self.nombre_archivo = fd.asksaveasfilename(title = "Guardar como", filetypes=(("xml files", "*.xml"), ("todos los archivos", "*.*")))
+        if self.nombre_archivo != "":
+            archivo = open(self.nombre_archivo, "w", encoding="utf-8")
+            archivo.write(self.scrolledtext1.get("1.0", tk.END))
+            archivo.close()
+            mb.showinfo("información", "El archivo se guardó correctamente")
+        
+    def guardar(self):
+        archivo = open(self.nombre_archivo, "w", encoding="utf-8")
+        archivo.write(self.scrolledtext1.get("1.0", tk.END))
+        archivo.close()
+        mb.showinfo("información", "El archivo se guardó correctamente")
+    
+    def abrir(self):
+        self.nombre_archivo = fd.askopenfilename(title="Seleccione el archivo", filetypes=(("xml file", "*.xml"), ("todos los arhivos", "*.*")))
+        if self.nombre_archivo != " ":
+            archivo = open(self.nombre_archivo, "r", encoding="utf-8")
+            contenido = archivo.read()
+            archivo.close()
+            self.scrolledtext1.delete("1.0", tk.END)
+            self.scrolledtext1.insert("1.0", contenido)
+        
+aplicacion = VentanaPrincipal()
